ff1b99c4c18fb8b1978ece844180599dce035ac4
Chase Berry <chase@bryx.com>
2014-08-27 17:59:34 -0400

    Added the javadocs for the Parameter Class

--- src/main/java/edu/csh/chase/RestfulAPIConnector/Parameter.java
+++ src/main/java/edu/csh/chase/RestfulAPIConnector/Parameter.java
@@ -5,8 +5,8 @@ package edu.csh.chase.RestfulAPIConnector;
  */
 public class Parameter {
 
-    public static final int URLPARAMETER = 0;//get
-    public static final int FORMDATA = 1;//post
+    public static final int URLPARAMETER = 0;
+    public static final int FORMDATA = 1;
     public static final int HEADER = 2;
     public static final int JSONBODY = 3;
 
@@ -14,6 +14,18 @@ public class Parameter {
     private String value;
     private final int methodType;
 
+    /**
+     * Defines a new Parameter to send with a URL Request
+     *
+     * @param key A String key for the parameter
+     * @param value A String value for the parameter
+     * @param methodType An int definition of the method type
+     *                   Parameter.URLPARAMETER - A parameter added to the end of the url as ?key=value
+     *                   Parameter.FORMDATA - A paramter in a POST form. Only works for POST requests
+     *                   Parameter.HEADER - A URL Encoded header
+     *                   Parameter.JSONBODY - Raw body in JSON Format
+     * @throws RestAPIParemeterException If you pass an invalid Parameter type
+     */
     public Parameter(final String key, String value, int methodType) throws RestAPIParemeterException{
         if(methodType < 0 || methodType > 3){
             throw new RestAPIParemeterException(methodType + " is not a valid parameter type.");
@@ -24,26 +36,50 @@ public class Parameter {
     }
 
 
+    /**
+     *
+     * @return They key associted with this Parameter
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     *
+     * @return The value associted with this Parameter
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets the value of this Parameter
+     * @param value
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
+    /**
+     *
+     * @return The int method type
+     */
     public int getMethodType() {
         return methodType;
     }
 
+    /**
+     *
+     * @return A string defination of this Parameter
+     */
     public String toString(){
         return "Paremter of type " + getParemeterType() + " with data as key:value, " + key + ":" + value;
     }
 
+    /**
+     *
+     * @return A string representation of the method type
+     */
     public String getParemeterType() {
         switch (methodType) {
             case 0: return "URL";
