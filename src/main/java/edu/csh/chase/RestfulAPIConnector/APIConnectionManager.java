0be6166f4c0320db6c09e4623c13f3999418dd2e
Chase Berry <chase@bryx.com>
2014-08-27 18:21:43 -0400

    Added javadocs on the APIConnectionManager

--- src/main/java/edu/csh/chase/RestfulAPIConnector/APIConnectionManager.java
+++ src/main/java/edu/csh/chase/RestfulAPIConnector/APIConnectionManager.java
@@ -21,21 +21,47 @@ public class APIConnectionManager {
     private Context parent;
     private String url;
 
-    public APIConnectionManager(Context c, String url) {
-        parent = c;
+    /**
+     * Creates an APIConnectionManager to interface with the RestAPIConnector
+     *
+     * @param context The context of the parent
+     * @param url The base URL for the connection
+     */
+    public APIConnectionManager(Context context, String url) {
+        parent = context;
         this.url = url;
     }
 
-    public APIConnectionManager(Context c) {
-        parent = c;
+    /**
+     * Creates an APIConnectionManager to interface with the RestAPIConnector
+     *
+     * @param context The context of the parent
+     */
+    public APIConnectionManager(Context context) {
+        this(context, "");
     }
 
+    /**
+     * Sets the default URL for requests
+     * @param url The base URL for the connection
+     */
     public void setDefaultUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Executes a request to the requestURL
+     * Will call the passed RestAPIListener success or failure depending on status code
+     *
+     * @param runner A RestAPIListener to process the returned data when the request finishes
+     * @param url The url for the request
+     * @param method The request method to make the request
+     * @param extra A string extra that can be accessed from the RestAPIListener when the request is finished
+     * @param parameters A vararg of Parameters to send with the request
+     * @throws InvalidMethodTypeException if the method is not a valid method type defined in RestAPIConnector
+     */
     public void execute(RestAPIListener runner, String url, int method, String extra, Parameter... parameters)
-            throws InvalidMethodTypeException {
+        throws InvalidMethodTypeException {
         if (!isNetworkOnline()) {
             return;
         }
@@ -52,26 +78,72 @@ public class APIConnectionManager {
         connector.execute();
     }
 
+    /**
+     * Executes a request to the baseURL
+     * Will call the passed RestAPIListener success or failure depending on status code
+     *
+     * @param runner A RestAPIListener to process the returned data when the request finishes
+     * @param method The request method to make the request
+     * @param parameters A vararg of Parameters to send with the request
+     * @throws InvalidMethodTypeException if the method is not a valid method type defined in RestAPIConnector
+     */
     public void execute(RestAPIListener runner, int method, Parameter... parameters)
-            throws InvalidMethodTypeException {
+        throws InvalidMethodTypeException {
         if (url == null) {
             return; //invalid URL
         }
         execute(runner, this.url, method, parameters);
     }
 
+    /**
+     * Executes a request to the requestURL
+     * Will call the passed RestAPIListener success or failure depending on status code
+     *
+     * @param runner A RestAPIListener to process the returned data when the request finishes
+     * @param url The url for the request
+     * @param method The request method to make the request
+     * @param parameters A vararg of Parameters to send with the request
+     * @throws InvalidMethodTypeException if the method is not a valid method type defined in RestAPIConnector
+     */
     public void execute(RestAPIListener runner, String url, int method, Parameter... parameters)
-            throws InvalidMethodTypeException {
+        throws InvalidMethodTypeException {
         execute(runner, url, method, null, parameters);
     }
 
+    /**
+     * Executes a request to the baseURL with the defined endpoint
+     * Will call the passed RestAPIListener success or failure depending on status code
+     *
+     * @param runner A RestAPIListener to process the returned data when the request finishes
+     * @param endPoint The URL Endpoint to make the request against.
+     *                 With a base url of http://example.com/api(/)
+     *                 and endpoint of "(/)messages/15(/)"
+     *                 will become "http://example.com/api/messages/15"
+     * @param method The request method to make the request
+     * @param parameters A vararg of Parameters to send with the request
+     * @throws InvalidMethodTypeException if the method is not a valid method type defined in RestAPIConnector
+     */
     public void executeWithEndpoint(RestAPIListener runner, String endPoint, int method, Parameter... parameters)
-            throws InvalidMethodTypeException {
+        throws InvalidMethodTypeException {
         executeWithEndpoint(runner, endPoint, method, null, parameters);
     }
 
+    /**
+     * Executes a request to the baseURL with the defined endpoint
+     * Will call the passed RestAPIListener success or failure depending on status code
+     *
+     * @param runner A RestAPIListener to process the returned data when the request finishes
+     * @param endPoint The URL Endpoint to make the request against.
+     *                 With a base url of http://example.com/api(/)
+     *                 and endpoint of "(/)messages/15(/)"
+     *                 will become "http://example.com/api/messages/15"
+     * @param method The request method to make the request
+     * @param extra A string extra that can be accessed from the RestAPIListener when the request is finished
+     * @param parameters A vararg of Parameters to send with the request
+     * @throws InvalidMethodTypeException if the method is not a valid method type defined in RestAPIConnector
+     */
     public void executeWithEndpoint(RestAPIListener runner, String endPoint, int method, String extra, Parameter... parameters)
-    throws InvalidMethodTypeException{
+        throws InvalidMethodTypeException{
         if (this.url == null) {
             return;//throw error?
         }
@@ -84,6 +156,10 @@ public class APIConnectionManager {
         execute(runner, url + endPoint, method, extra, parameters);
     }
 
+    /**
+     * A convience method for network connection
+     * @return true if there is some form of internet access, false otherwise
+     */
     public boolean isNetworkOnline() {
         boolean status = false;
         try {
