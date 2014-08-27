a2f889e9d0c3d01b888b21352ea4d4ec9dda940d
Chase Berry <chase@bryx.com>
2014-08-27 18:43:04 -0400

    Added Javadocs for the success/failure methods. Gona rework on the listener works

--- src/main/java/edu/csh/chase/RestfulAPIConnector/RestAPIListener.java
+++ src/main/java/edu/csh/chase/RestfulAPIConnector/RestAPIListener.java
@@ -12,10 +12,24 @@ public abstract class RestAPIListener {
     private String extra;
     private int statusCode;
 
+    /**
+     * The success function
+     * This function is called when an request returns with a status code of 200 - 299
+     *
+     * @param data
+     * @param statusCode
+     */
     public abstract void success(JSONWrapper data, final int statusCode);
 
+    /**
+     * The failure function
+     * This function is called when a request returns with a status code not between 200 and 299
+     *
+     * @param statusCode
+     */
     public abstract void failure(final int statusCode);
 
+    
     public void start() {
         if(statusCode >= 200 || statusCode <= 299){
             success(data, statusCode);
