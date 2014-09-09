3cf85fb0f928e5650ef71d06e3a9af430760691e
chase <chase@bryx.com>
2014-09-09 11:54:48 -0400

    Removed the now-useless line JSONParam type indentifiers

--- src/main/java/edu/csh/chase/RestfulAPIConnector/JSONParameter.java
+++ src/main/java/edu/csh/chase/RestfulAPIConnector/JSONParameter.java
@@ -9,15 +9,7 @@ import java.lang.String;
 
 public class JSONParameter extends Parameter{
 
-    public static final int JSONSTRINGBODY = 0;
-    public static final int JSONOBJECTPARAMETER = 1;
-    public static final int JSONARRAYPARAMETER = 2;
-    public static final int JSONINTPARAMETER = 3;
-    public static final int JSONBOOLEANPARAMETER = 4;
-    public static final int JSONDOUBLEPARAMETER = 5;
-    public static final int JSONLONGPARAMETER = 6;
     Object value;//either JSONObject or JSONArray;
-    private int jsonParameterType = 0;
 
     public JSONParameter(final String key, String value) throws RestAPIParemeterException{
         super(key, value, Parameter.JSONBODY);
@@ -25,38 +17,32 @@ public class JSONParameter extends Parameter{
 
     public JSONParameter(final String key, JSONObject value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
-        jsonParameterType = JSONOBJECTPARAMETER;
         this.value = value;
     }
 
     public JSONParameter(final String key, JSONArray value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
-        jsonParameterType = JSONARRAYPARAMETER;
         this.value = value;
     }
 
     public JSONParameter(final String key, boolean value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
         this.value = new Boolean(value);
-        jsonParameterType = JSONBOOLEANPARAMETER;
     }
 
     public JSONParameter(final String key, int value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
         this.value = new Integer(value);
-        jsonParameterType = JSONINTPARAMETER;
     }
 
     public JSONParameter(final String key, double value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
         this.value = new Double(value);
-        jsonParameterType = JSONDOUBLEPARAMETER;
     }
 
     public JSONParameter(final String key, long value) throws RestAPIParemeterException{
         super(key, "", Parameter.JSONBODY);
         this.value = new Long(value);
-        jsonParameterType = JSONLONGPARAMETER;
     }
 
     public int getJsonParameterType(){
